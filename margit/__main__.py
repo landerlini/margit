b0a23fbc3746b537c7f9d730dc7050f4336fa93b
Lucio Anderlini <Lucio.Anderlini@cern.ch>
2021-09-02 18:53:49 +0200

    fixed main

--- margit/__main__.py
+++ margit/__main__.py
@@ -2,12 +2,15 @@ from argparse import ArgumentParser
 import sys 
 import margit
 
-parser = ArgumentParser (usage = "margit <command>")
+def main():
+  parser = ArgumentParser (usage = "margit <command>")
 
-parser.add_argument ("command", help="Command", choices=margit.cli_commands) 
+  parser.add_argument ("command", help="Command", choices=margit.cli_commands) 
 
-args = parser.parse_args(sys.argv[1:2])
+  args = parser.parse_args(sys.argv[1:2])
 
-margit.cli_commands [args.command] (sys.argv[2:]) 
+  margit.cli_commands [args.command] (sys.argv[2:]) 
 
+if __name__ == '__main__':
+  main() 
 
